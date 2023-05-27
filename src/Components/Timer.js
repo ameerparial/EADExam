578dddfe2a8422f0e566bc26c42c0e37d34a61e4
ameerparial <khuhroameerhamza4@gmail.com>
2023-05-27 17:40:07 +0500

    Start Counter method is created

--- src/Components/Timer.js
+++ src/Components/Timer.js
@@ -12,6 +12,14 @@ export default class Timer extends Component {
         this.setState({ counter: 10 });
     }
 
+    startCounter = ()=> {
+        if (this.state.counter > 0) {
+          this.timer = setInterval(this.countDown, 1000);
+        }
+    }
+
+
+
 
 
   render() {
