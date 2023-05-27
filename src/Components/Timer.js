a31b51d6596c5b775787c34df6887731c8ab7f5e
ameerparial <khuhroameerhamza4@gmail.com>
2023-05-27 17:45:02 +0500

    Time Functionality added

--- src/Components/Timer.js
+++ src/Components/Timer.js
@@ -7,24 +7,34 @@ export default class Timer extends Component {
         this.state = {counter:10};
     }
 
-
     componentDidMount() {
         this.setState({ counter: 10 });
     }
 
     startCounter = ()=> {
         if (this.state.counter > 0) {
-          this.timer = setInterval(this.countDown, 1000);
+          this.timer = setInterval(this.decreaseCounter, 1000);
         }
     }
 
-
-
-
+    decreaseCounter = ()=> {
+        let counter = this.state.counter - 1;
+        this.setState({
+            counter: counter
+        });
+        
+        if (counter == 0) { 
+          clearInterval(this.timer);
+          alert("Times UP");
+        }
+      }
 
   render() {
     return (
-      <div>Timer</div>
+      <div>
+        <h1>{this.state.counter}</h1>
+        <button onClick={this.startCounter}>Start</button>
+      </div>
 
     )
   }
